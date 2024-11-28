fdec56f65c5a3491ec041a91651607dedc315f42
jypark6659 <jypark6659@naver.com>
2024-11-28 15:09:52 +0900

    implement hit_wall

--- implements.py
+++ implements.py
@@ -91,12 +91,10 @@ class Ball(Basic):
             self.dir = 360 - self.dir + random.randint(-5, 5)
 
     def hit_wall(self):
-        # ============================================
-        # TODO: Implement a service that bounces off when the ball hits the wall
-        pass
-        # 좌우 벽 충돌
-        
-        # 상단 벽 충돌
+        if self.rect.left <= 0 or self.rect.right >= config.display_dimension[0]:
+            self.dir = 180 - self.dir
+        if self.rect.top <= 0:
+            self.dir = 360 - self.dir
     
     def alive(self):
         # ============================================
